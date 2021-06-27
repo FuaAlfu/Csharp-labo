16935f452e443515d1134573551463720e6f5e73
FuaAlfu <mr.fahadu2@gmail.com>
2021-06-27 22:53:01 +0300

    add

---
+++ 001-checking-ages/Person.cs
@@ -0,0 +1,14 @@
+using System;
+namespace _001_checking_ages
+{
+    public class Person
+    {
+        public int Age {get;set;}
+       public Person(int age)
+        {
+            this.Age = age;
+        }
+
+        
+    }
+}
--- 001-checking-ages/Program.cs
+++ 001-checking-ages/Program.cs
@@ -6,7 +6,26 @@ namespace _001_checking_ages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Person p1 = new Person(24);
+           Person p2 = new Person(20);
+           AgeChecking("p1:",p1.Age);
+           AgeChecking("p2:",p2.Age);
+
+           System.Console.WriteLine(p1.Age);
+
+        }
+
+        static void AgeChecking(string s,int age)
+        {
+             int passAge = 20;
+             if(passAge >= age)
+             {
+                 Console.WriteLine(s+" you shall not pass");
+             }
+             else
+             {
+                  Console.WriteLine(s+" welcom!");
+             }
         }
     }
 }
