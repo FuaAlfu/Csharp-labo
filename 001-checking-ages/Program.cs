21a03d7572f51573fef4a9c832da4fcf17b81831
FuaAlfu <mr.fahadu2@gmail.com>
2021-06-27 22:12:37 +0300

    add

---
+++ 001-checking-ages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001_checking_ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
