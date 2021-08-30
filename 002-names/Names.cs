d196f12928ed9eca856d030cefb045ac241d4c37
FuaAlfu <mr.fahadu2@gmail.com>
2021-08-30 20:59:21 +0300

    add

---
+++ 002-names/Names.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _002_names
+{
+    public class Names
+    {
+        public string Name {get;set;}
+       public Names(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ 002-names/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _002_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Name n1 = new Names("998711");
+            Name n2 = new Names("991701");
+
+           Console.WriteLine(n2.Name);
+           CheckID("m1", n1.Name);
+        }
+
+        static void CheckID(string s,string id)
+        {
+            string idCheck = "998711";
+             if(idCheck != id)
+             {
+                 Console.WriteLine(s+" not pass");
+             }
+             else
+             {
+                  Console.WriteLine(s+" welcom!");
+             }
+        }
+    }
+}
