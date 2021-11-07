34c21d59e9f8c6860000e6ea800c2d44354c18f4
FuaAlfu <mr.fahadu2@gmail.com>
2021-11-07 19:46:34 +0300

    add

---
+++ 006-stackAndHeap/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _006_stackAndHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Book b1 = new Book();
+            {
+                BookDescription = "fantasy and glancy";
+                BookTitle = "king's drink";
+                PageNumber = 221;
+            };
+
+            Book b2 = b1;
+
+            Console.WriteLine(b1.Read());
+            Console.WriteLine(b2.Read());
+            Console.WriteLine("uu");
+        }
+    }
+
+    class Book
+    {
+      public  string BookDescription {get; set;}
+      public  string BookTitle {get; set;}
+      public  int PageNumber {get; set;}
+
+        public string Read()
+        {
+            return String.Format("{0}: {1}, pp. {2}", BookDescription, BookTitle,PageNumber);
+        }
+
+    }
+
+    
+}
---
+++ 007-randomNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _007_randomNumber
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+             Console.WriteLine(RandomNumber(0,256));
+        }
+        
+         // generates a random number within a range.
+            public static int RandomNumber(int min, int max)  
+            {  
+               Random random = new Random();
+               return random.Next(min, max);  
+            }  
+    }
+}
