ea19fb308ced6cd7aa689649be5eacfb16838e10
FuaAlfu <mr.fahadu2@gmail.com>
2021-11-25 03:33:19 +0300

    add

---
+++ 008-generate-random-array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+
+namespace _008_generate_random_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Random r = new Random();
+            int valSize = 30;
+            int[] array = new int[valSize];
+            for(int i = 0; i < valSize; i++)
+            {
+            array[i] = r.Next(0,array.Length);
+            Console.WriteLine("random: "+ array[i]);
+            }
+        }
+    }
+}
