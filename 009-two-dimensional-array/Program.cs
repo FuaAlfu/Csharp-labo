b99f844d1d4b1f2a44a381b717ba4185f451724a
FuaAlfu <mr.fahadu2@gmail.com>
2021-11-26 03:21:14 +0300

    add

---
+++ 009-two-dimensional-array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _009_two_dimensional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int valSize = 30;
+            int[,] array = new int[valSize,valSize];
+            for(int i = 0; i < valSize; i++)
+            {
+                for(int j = 0; j < valSize; j++)
+                {
+                      array[i,j] = r.Next(0,array.Length);
+                      Console.WriteLine("random: "+ array[i,j]);
+                }
+             Console.WriteLine(" ");
+            }
+        }
+    }
+}
