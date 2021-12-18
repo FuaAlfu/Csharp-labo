9a946af49d73667e657ce36ebf4745d2a5768a5b
FuaAlfu <mr.fahadu2@gmail.com>
2021-12-18 15:39:14 +0300

    add

---
+++ 010-get-middle-element-inArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace _010_get_middle_element_inArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[]{11,2,41,97};
+            int mid = array[array.Length / 2];
+            Console.WriteLine(mid);
+        }
+    }
+}
