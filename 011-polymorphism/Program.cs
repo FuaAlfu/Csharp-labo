0004a6102f25f60ac3de903b7f7917621094abb4
FuaAlfu <mr.fahadu2@gmail.com>
2022-01-16 00:27:42 +0300

    polymorphism

--- 011-polymorphism/Program.cs
+++ 011-polymorphism/Program.cs
@@ -11,12 +11,29 @@ namespace _011_polymorphism
             int number1 = 33;
             int number2 = 44;
 
+            //------------(override)
             MainObject m = new MainObject();
             m = new SubObject();
            Console.WriteLine("adding numbers: "+ m.Adding(number1,number2));
            //System.Console.WriteLine("boo");
 
+           //-----------(overload)
+           Program p = new Program();
+
+           Console.WriteLine(p.Add(number1,number2));
+           Console.WriteLine(p.Add(number1,number2,number1));
+
             Console.ReadLine();
         }
+
+         public int Add(int a, int b)
+           {
+               return (a + b);
+           }
+
+          public int Add(int a, int b, int c)
+           {
+               return a + b + c;
+           }
     }
 }
